f78951be902efc9b16eec848da4e2ecaf599e4db
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-10-13 22:18:45 -0400

    Fixed issue with entrypoint.sh not recoginizing variable name for folders

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -3,13 +3,14 @@
 folders=("include" "includes" "src" "source" "sources" "test" "tests")
 for folder in "${folders[@]}"
 do
-    echo $(folder)
-    if [ -d ${folder} ]; then
+    echo "$folder"   # Directly use the variable
+    if [ -d "$folder" ]; then
         command="cpplint --filter=-build/include_subdir,-legal/copyright --recursive ${folder}/*"
-        echo $(command)
-        if ${command} | grep "error"; then
+        echo "$command"   # Directly use the variable
+        if $command | grep "error"; then
             echo "::ERROR:: Review code"
             exit 1
         fi
     fi
 done
+
